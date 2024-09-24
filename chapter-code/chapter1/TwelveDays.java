d620b086af3e8b50eec310ab2ed09ebc3a75c66f
julian-patterson <julianedwardpatterson@gmail.com>
2024-09-24 12:38:09 -0400

    Finished chp 1

--- chapter-code/chapter1/TwelveDays.java
+++ chapter-code/chapter1/TwelveDays.java
@@ -13,20 +13,26 @@ package chapter1;
 
 /**
  * Outputs the text of the poem "The Twelve Days of Christmas"
- * to the console. The code leverages the natural recursion in 
+ * to the console. The code leverages the natural recursion in
  * the structure of the poem.
  */
-public class TwelveDays
-{
-	public static void main(String[] args)
-	{
+public class TwelveDays {
+	private static boolean isDigit;
+
+	public static void main(String[] args) {
+		if (args.length > 0 && args[0].equals("digit")) {
+			isDigit = true;
+		}
 		System.out.println(poem());
 	}
-	
-	static String[] DAYS = {"first", "second", "third", "fourth",
-							"fifth", "sixth", "seventh", "eighth",
-							"ninth", "tenth", "eleventh", "twelfth"};
-	
+
+	static String[] DAYS = { "first", "second", "third", "fourth",
+			"fifth", "sixth", "seventh", "eighth",
+			"ninth", "tenth", "eleventh", "twelfth" };
+
+	static String[] DAYS_DIGITS = { "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th",
+			"12th" };
+
 	static String[] GIFTS = {
 			"a partridge in a pear tree",
 			"two turtle doves",
@@ -41,41 +47,38 @@ public class TwelveDays
 			"eleven Pipers Piping",
 			"twelve Drummers Drumming"
 	};
-	
+
 	/*
-     * Returns the first line in the verse for a given day.
+	 * Returns the first line in the verse for a given day.
 	 */
-	static String firstLine(int day)
-	{
-	  return "On the " + DAYS[day] +
-	    " day of Christmas my true love sent to me:\n";
-	}
-	
-	/*
-     * Returns a string that lists all the gifts received on a given
-     * day.
-	 */
-	static String allGifts(int day)
-	{
-		if( day == 0 ) 
-		{ 
-			return "and " + GIFTS[0]; 
+	static String firstLine(int day) {
+		if (isDigit) {
+			return "On the " + DAYS_DIGITS[day] +
+					" day of Christmas my true love sent to me:\n";
 		}
-		else 
-		{ 
-			return GIFTS[day] + "\n" + allGifts(day-1);	
+		return "On the " + DAYS[day] +
+				" day of Christmas my true love sent to me:\n";
+	}
+
+	/*
+	 * Returns a string that lists all the gifts received on a given
+	 * day.
+	 */
+	static String allGifts(int day) {
+		if (day == 0) {
+			return "and " + GIFTS[0];
+		} else {
+			return GIFTS[day] + "\n" + allGifts(day - 1);
 		}
 	}
-	
+
 	/*
-	 * Returns the text of the entire poem. 
+	 * Returns the text of the entire poem.
 	 */
-	static String poem()
-	{
+	static String poem() {
 		String poem = firstLine(0) + GIFTS[0] + "\n\n";
-		for( int day = 1; day < 12; day++ )
-		{ 
-			poem += firstLine(day) + allGifts(day) + "\n\n"; 
+		for (int day = 1; day < 12; day++) {
+			poem += firstLine(day) + allGifts(day) + "\n\n";
 		}
 		return poem;
 	}
